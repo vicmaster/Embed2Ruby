67b9641c6b44f35c65614e4cca4afbb63b713aae
Alejandro <velazquezgaspar16@hotmail.com>
2012-03-23 17:57:24 -0600

    second one commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Languagescape::Application.config.secret_token = '832f6eb453587b2744aaa9da04217343424c944de243ada9c36282a34ead2cb4c9cc603fb097a22a70efa48119f95e671b1e4b646e48b0ca70b1a72a8eb88c91'
